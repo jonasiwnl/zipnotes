5a8cca87294ffe732b9236105e0860666fdc329e
arael <jonasg@umich.edu>
2023-11-28 16:30:58 -0500

    add bg and text input boxes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
-use web_sys::{wasm_bindgen::{JsCast, UnwrapThrowExt}, HtmlTextAreaElement};
+use web_sys::{wasm_bindgen::{JsCast, UnwrapThrowExt}, HtmlTextAreaElement, HtmlInputElement};
 use yew::prelude::*;
 
 #[function_component]
 fn App() -> Html {
     let window = web_sys::window().unwrap();
     let storage = window.local_storage().unwrap().unwrap();
-    let stored_notes = storage.get_item("notes").unwrap().unwrap_or_default();
 
-    // Initialize default state with stored notes.
+    let stored_notes = storage.get_item("notes").unwrap().unwrap_or_default();
+    let stored_bg_color = storage.get_item("bg_color").unwrap().unwrap_or("#181926".to_string());
+    let stored_text_color = storage.get_item("text_color").unwrap().unwrap_or("#cad3f5".to_string());
+
+    // Initialize default state with stored data.
     let notes = use_state(|| stored_notes);
+    let bg_color = use_state(|| stored_bg_color);
+    let text_color = use_state(|| stored_text_color);
+
+    let set_storage = |key: &str, value: &str| {
+        let window = web_sys::window().unwrap();
+        let storage = window.local_storage().unwrap().unwrap();
+        storage.set_item(key, value).unwrap_throw();
+    };
 
     // Handler for when the user types.
-    let type_handler = Callback::from({
+    // TODO how to avoid this duplication
+    let notes_type_handler = Callback::from({
         let notes = notes.clone();
         move |input_event: InputEvent| {
             let target: HtmlTextAreaElement = input_event
@@ -19,14 +31,44 @@ fn App() -> Html {
                 .unwrap_throw()
                 .dyn_into()
                 .unwrap_throw();
+
             notes.set(target.value());
-            storage.set_item("notes", &target.value()).unwrap_throw();
+            set_storage("notes", &target.value());
+        }
+    });
+    let bg_type_handler = Callback::from({
+        let bg_color = bg_color.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlInputElement = input_event
+                .target()
+                .unwrap_throw()
+                .dyn_into()
+                .unwrap_throw();
+
+            bg_color.set(target.value());
+            set_storage("bg_color", &target.value());
+        }
+    });
+    let text_type_handler = Callback::from({
+        let text_color = text_color.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlTextAreaElement = input_event
+                .target()
+                .unwrap_throw()
+                .dyn_into()
+                .unwrap_throw();
+
+            text_color.set(target.value());
+            set_storage("text_color", &target.value());
         }
     });
 
     html! {
         <div>
-            <textarea oninput={type_handler} value={(&*notes).clone()} spellcheck="false" />
+            <h1>{ "ZipNotes" }</h1>
+            <input type="text" oninput={bg_type_handler} value={(&*bg_color).clone()} />
+            <input type="text" oninput={text_type_handler} value={(&*text_color).clone()} />
+            <textarea oninput={notes_type_handler} value={(&*notes).clone()} spellcheck="false" />
             <style>
                 {r#"
                     textarea {
